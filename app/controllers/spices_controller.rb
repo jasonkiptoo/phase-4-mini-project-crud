a4b53925e23705a7c84db156c2db0375142407bf
JasonKiptoo <kiptoojason@gmail.com>
2022-12-02 13:59:41 +0300

    task completed

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,44 +1,39 @@
+
 class SpicesController < ApplicationController
   def index
-    spices = Spice.all
-    render json: spices
-  end
-
-  def show
-    spices = Spice.find_by(id: params[:id])
-     if spices
+      spices = Spice.all
       render json: spices
-     else
-      render json: {error: "spice not found"}, status: :not_found
-     end
   end
 
   def create
-    spice =Spice.create(spice_params)
-    render json: spice, status: :created
-  end
-
-  def destroy
-    spice = Spice.find_by(id: params[:id])
-    if spice
-        spice.destroy
-        head :no_content
-    else
-      render json: {error: "Spice not found"}
-    end
+      spice = Spice.create(spice_params)
+      render json: spice, status: :created
   end
 
   def update
-    spice = spice.find_by(id: params[:id])
-    if spice
-      spice.update(spice_params)
-      render json: spice
-    else
-      render json: {error: "Spice updated"}, status: :updated
-    end
+      spice = Spice.find_by(id: params[:id])
+      if spice
+         spice.update(spice_params)
+         render json: spice
+       else
+         render json: { error: "Spice not found" }, status: :not_found
+      end
   end
 
+  def destroy
+      spice = Spice.find_by(id: params[:id])
+      if spice
+      spice.destroy
+      head :no_content
+      else
+      render json: { error: "Spice not found" }, status: :not_found
+      end
+  end
+
+
+  private
+
   def spice_params
-    params.permit(:title, :image, :description, :notes, :rating)
+      params.permit(:title, :image, :description, :notes, :rating)
   end
 end
